5aeb5ea131ac197bc0235a7848e13e57b442208a
Bob Fang <bob.fang.london@gmail.com>
2024-07-10 22:26:21 -0400

    update

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,11 @@
 set_languages("c++20")
 
+
+
 target("test")
     set_kind("binary")
     add_files("test.cpp")
+    add_cxflags("-Wall", "-Wextra", "-Werror", "-fno-slp-vectorize")
     -- Set optimization level based on the build mode
     if is_mode("debug") then
         set_optimize("none")
