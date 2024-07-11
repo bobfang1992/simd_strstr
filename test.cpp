878b87aa0e1568bbe33229a25edd0cdaeaedf0a9
Bob Fang <bob.fang.london@gmail.com>
2024-07-10 21:18:20 -0400

    update

--- test.cpp
+++ test.cpp
@@ -71,6 +71,78 @@ int simd_strstr(std::string_view str, std::string_view substr) {
   return -1;
 }
 
+int kmp_strstr(std::string_view str, std::string_view substr) {
+  size_t str_len = str.size();
+  size_t substr_len = substr.size();
+
+  if (substr_len > str_len) {
+    return -1;
+  }
+
+  if (substr_len == 0) {
+    return 0;
+  }
+
+  std::vector<size_t> lps(substr_len, 0);
+  size_t len = 0;
+  size_t i = 1;
+  while (i < substr_len) {
+    if (substr[i] == substr[len]) {
+      len++;
+      lps[i] = len;
+      i++;
+    } else {
+      if (len != 0) {
+        len = lps[len - 1];
+      } else {
+        lps[i] = 0;
+        i++;
+      }
+    }
+  }
+
+  size_t j = 0;
+  i = 0;
+  while (i < str_len) {
+    if (str[i] == substr[j]) {
+      i++;
+      j++;
+    }
+    if (j == substr_len) {
+      return i - j;
+    } else if (i < str_len && str[i] != substr[j]) {
+      if (j != 0) {
+        j = lps[j - 1];
+      } else {
+        i++;
+      }
+    }
+  }
+
+  return -1;
+}
+
+int naive_strstr(std::string_view str, std::string_view substr) {
+  size_t str_len = str.size();
+  size_t substr_len = substr.size();
+
+  if (substr_len > str_len) {
+    return -1;
+  }
+
+  if (substr_len == 0) {
+    return 0;
+  }
+
+  for (size_t i = 0; i <= str_len - substr_len; i++) {
+    if (str.substr(i, substr_len) == substr) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
 int benchmark(const std::string &str, const std::string &substr,
               size_t seconds_to_run,
               std::function<int(std::string_view, std::string_view)> func) {
@@ -102,7 +174,7 @@ int benchmark_simd_vs_std(const std::string &str, const std::string &substr,
   auto result_std =
       benchmark(str, substr, seconds_to_run,
                 [](std::string_view str, std::string_view substr) {
-                  return str.find(substr);
+                  return naive_strstr(str, substr);
                 });
   if (result_simd != result_std) {
     std::cout << "Mismatch: " << result_simd << " vs " << result_std
@@ -115,6 +187,10 @@ int main() {
   std::string str = read_file_into_string("/Users/bobfang/code/neon/sqlite3.c");
   std::string substr = "sqlite3_initialize";
 
+  benchmark_simd_vs_std(str, substr, 1);
+
+  str = "hello this is a new and interesting world, where we celebrate the harmony enjoyed by all human being";
+  substr = "world";
   benchmark_simd_vs_std(str, substr, 1);
   return 0;
 }
