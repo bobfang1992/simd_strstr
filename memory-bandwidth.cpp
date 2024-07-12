b30247187bd95d31cefd154a5257ed48bee1e8a3
Bob Fang <dorafmon@gmail.com>
2024-07-13 00:46:14 +0100

    save

--- memory-bandwidth.cpp
+++ memory-bandwidth.cpp
@@ -8,29 +8,32 @@
 #include <ctime>
 #include <fmt/core.h>
 
-constexpr int64_t kArraySize = 1024 * 1024;
+constexpr int64_t kArraySize = 1024 * 1024 * 32;
 
 int8_t rand_int() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int8_t> dis(0, 12);
-    return dis(gen);
+    static int i = 0; 
+    return i++ % 10;
 }
 
-std::array<int8_t, kArraySize> create_array_for_test() {
-    std::array<int8_t, kArraySize> arr;
+std::vector<int8_t> create_array_for_test() {
+    std::vector<int8_t> arr;
+    arr.reserve(kArraySize);
     for (int64_t i = 0; i < kArraySize; i++) {
-        arr[i] = rand_int();
+        arr.push_back(rand_int());
     }
     return arr;
 }
 
-int sum_sequential(const std::array<int8_t, kArraySize>& arr) {
+int sum_sequential(const std::vector<int8_t>& arr) {
     int sum = 0;
     for (int64_t i = 0; i < kArraySize; i++) {
         sum += arr[i];
     }
     return sum;
+}
+
+int std_sum(const std::vector<int8_t>& arr) {
+    return std::accumulate(arr.begin(), arr.end(), 0);
 }
 
 void print_with_timestamp(const std::string& message) {
@@ -51,25 +54,28 @@ void print_with_timestamp(const std::string& message) {
 }
 
 
-int run_benchmark(const std::array<int8_t, kArraySize>& arr, size_t iterations, std::function<int(const std::array<int8_t, kArraySize>&)> func) {
-    std::chrono::duration<double, std::micro> total = std::chrono::duration<double, std::milli>::zero();
+int run_benchmark(const std::vector<int8_t>& arr, size_t iterations, std::function<int(const std::vector<int8_t>&)> func) {
+    std::chrono::duration<double, std::milli> total = std::chrono::duration<double, std::milli>::zero();
     for(size_t i = 0; i < iterations; i++) {
         auto start = std::chrono::high_resolution_clock::now();
         func(arr);
         auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double, std::micro> diff = end - start;
+        std::chrono::duration<double, std::milli> diff = end - start;
         total += diff;
+        std::cout << ".";
     }
+    std::cout << std::endl;
     double totalExecutionTime = total.count();
 
-    print_with_timestamp(fmt::format("Total execution time: {} micro seconds", totalExecutionTime));
+    print_with_timestamp(fmt::format("Total execution time: {} seconds", totalExecutionTime / 1000));
 
     // Calculate the number of bytes processed
-    double totalBytesProcessed = kArraySize * sizeof(int8_t) * iterations;
+    double totalBytesProcessed = arr.size() * sizeof(int8_t) * iterations;
+    print_with_timestamp(fmt::format("Total bytes processed: {}", totalBytesProcessed));
+    
     double totalBytesProcessedInGB = totalBytesProcessed / (1024 * 1024 * 1024);
-
     // Calculate the throughput in GB/s
-    double throughput = totalBytesProcessedInGB / (totalExecutionTime / 1000000);
+    double throughput = totalBytesProcessedInGB / (totalExecutionTime / 1000);
 
     print_with_timestamp(fmt::format("Throughput: {} GB/s", throughput));
 
@@ -83,4 +89,7 @@ int main() {
 
     print_with_timestamp("-------- running sequential sum --------");
     run_benchmark(arr, 100, sum_sequential);
+
+    print_with_timestamp("-------- running std::accumulate --------");
+    run_benchmark(arr, 100, std_sum);
 }
